92e27df15f653ec5954c0e59f6bba010add95ffe
mehedi324 <159608752+mehedi324@users.noreply.github.com>
2024-10-18 18:50:43 +0600

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002401918856"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
 FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002067183105')).split()]
 
 # Don't Remove Credit Tg - @VJ_Botz
